aa183ab10dcea97fbdcb63d70dab57f8da49bc5b
ss <csperichon@gmail.com>
2022-08-16 21:02:16 -0300

    updated for users and login

--- app.js
+++ app.js
@@ -1,11 +1,14 @@
 const config = require('./utils/config')
 const express = require('express')
+require('express-async-errors')
 const app = express()
 const cors = require('cors')
 const blogsRouter = require('./controllers/blogs')
 const middleware = require('./utils/middleware')
 const logger = require('./utils/logger')
 const mongoose = require('mongoose')
+const usersRouter = require('./controllers/users')
+const loginRouter = require('./controllers/login')
 
 logger.info('connecting to', config.MONGODB_URI)
 
@@ -22,7 +25,12 @@ app.use(express.static('build'))
 app.use(express.json())
 app.use(middleware.requestLogger)
 
+app.use('/api/users', usersRouter)
+
+app.use('/api/login', loginRouter)
+
 app.use('/api/blogs', blogsRouter)
+app.use('/', blogsRouter)
 
 app.use(middleware.unknownEndpoint)
 app.use(middleware.errorHandler)
